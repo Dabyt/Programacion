725fefe1311ebb51b67e07f2f454725279ef08f2
alumne-DAM <alumne-DAM@172.16.26.91>
2018-12-13 16:32:40 +0100

    Empezando el coche

---
+++ David/src/pru03/E02/PRU03E02Cotxe_David_Sanchez.java
@@ -0,0 +1,59 @@
+package pru03.E02;
+
+public class PRU03E02Cotxe_David_Sanchez extends CotxeAbstracte implements InterfaceCotxe{
+	
+	protected EstatsMotorCotxe estatsmotor = EstatsMotorCotxe.Aturat;
+	
+	public PRU03E02Cotxe_David_Sanchez(String marca, String model, TipusCanvi tipuscanvi) {
+		super(marca, model, tipuscanvi);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void arrancarMotor() throws Exception {
+		
+	if (estatsmotor.equals(EstatsMotorCotxe.Aturat)) {
+		
+		estatsmotor = EstatsMotorCotxe.EnMarxa;
+		
+	} else {
+		
+		throw new IllegalArgumentException ("El coche ya esta encendido.");
+		
+	}
+		
+		
+		
+	}
+
+	@Override
+	public EstatsMotorCotxe comprovaMotor() {
+		
+		
+		return null;
+	}
+
+	@Override
+	public int getRevolucions() {
+		
+		return 0;
+	}
+
+	@Override
+	public void aturarMotor() throws Exception {
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ David/src/pru03/E02/PRU03E02TestCotxe_David_Sanchez.java
@@ -0,0 +1,5 @@
+package pru03.E02;
+
+public class PRU03E02TestCotxe_David_Sanchez {
+
+}
