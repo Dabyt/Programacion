b42d0a9e3a99706dcc9804855a7aafecb4480620
alumne-DAM <alumne-DAM@172.16.26.91>
2018-12-14 16:56:32 +0100

    Final

--- David/src/pru03/E02/PRU03E02Cotxe_David_Sanchez.java
+++ David/src/pru03/E02/PRU03E02Cotxe_David_Sanchez.java
@@ -1,60 +1,64 @@
 package pru03.E02;
 
 public class PRU03E02Cotxe_David_Sanchez extends CotxeAbstracte implements InterfaceCotxe{
-	
-	protected EstatsMotorCotxe estatsmotor = EstatsMotorCotxe.Aturat;
-	
-	public PRU03E02Cotxe_David_Sanchez(String marca, String model, TipusCanvi tipuscanvi, EstatsMotorCotxe estat) {
+
+	protected EstatsMotorCotxe estatsmotor;
+
+	public PRU03E02Cotxe_David_Sanchez(String marca, String model, TipusCanvi tipuscanvi) {
 		super(marca, model, tipuscanvi);
 		this.marca = marca;
-		
+		this.model = model;
+		this.tipuscanvi = tipuscanvi;
+		this.estatsmotor = EstatsMotorCotxe.Aturat;
+
 	}
 
 	@Override
 	public void arrancarMotor() throws Exception {
-		
-	if (estatsmotor.equals(EstatsMotorCotxe.Aturat)) {
-		
-		estatsmotor = EstatsMotorCotxe.EnMarxa;
-		
-	} else {
-		
-		throw new IllegalArgumentException ("El coche ya esta encendido.");
-		
-	}	
+
+		if (estatsmotor.equals(EstatsMotorCotxe.Aturat)) {
+
+			estatsmotor = EstatsMotorCotxe.EnMarxa;
+			System.out.println ("Coche en marcha.");
+
+		} else { 
+
+			throw new IllegalArgumentException ("El coche ya esta encendido.");
+
+		}	
 	}
 
-	@Override
+	@Override 
 	public EstatsMotorCotxe comprovaMotor() {
-		
+
 		return estatsmotor;
 	}
 
 	@Override
 	public int getRevolucions() {
-		
+
 		int revoluciones = 1;
-		
+
 		if (estatsmotor.equals(EstatsMotorCotxe.EnMarxa)) {
 			revoluciones = (int) (Math.random()*6500);
 		}
 		else if (estatsmotor.equals(EstatsMotorCotxe.EnMarxa)) {
-			
+
 			revoluciones = 0;
 		}	
-		
+
 		return revoluciones;
 	}
 
 	@Override
 	public void aturarMotor() throws Exception {
-	
+
 		if (estatsmotor.equals(EstatsMotorCotxe.EnMarxa)) {
-			
+
 			estatsmotor = EstatsMotorCotxe.Aturat;
-			
+
 		} else {
-			
+
 			throw new IllegalArgumentException ("El coche ya esta apagado.");		
 		}		
 	}
--- David/src/pru03/E02/PRU03E02TestCotxe_David_Sanchez.java
+++ David/src/pru03/E02/PRU03E02TestCotxe_David_Sanchez.java
@@ -1,5 +1,70 @@
 package pru03.E02;
 
+import java.util.Scanner;
+
 public class PRU03E02TestCotxe_David_Sanchez {
 
-}
+	public static void main (String[] args) throws Exception {
+
+		PRU03E02Cotxe_David_Sanchez micoche = new PRU03E02Cotxe_David_Sanchez("Citroen", "Renault", TipusCanvi.CanviAutomatic);	
+		int menu;
+		int n= 0;
+		Scanner teclado = new Scanner(System.in);	          
+
+		do {
+
+			System.out.println ("1- Arrancar coche");
+			System.out.println ("2- Estado del motor. ");
+			System.out.println ("3- Revoluciones del coche.");
+			System.out.println ("4- Apagar coche..");
+
+			menu = teclado.nextInt();
+
+			if (menu==1) {
+
+				micoche.arrancarMotor();
+
+			}
+			else if (menu==2) {
+
+				System.out.println(micoche.comprovaMotor()); 
+
+			}
+			else if (menu==3) {
+
+				System.out.println("Tus revoluciones son " + micoche.getRevolucions()); 
+
+			}
+			else if (menu==4) {
+				
+				micoche.aturarMotor();
+				System.out.println("Su coche se ha apagado, hasta la proxima."); 
+				n++;
+			}
+			
+			else{
+				System.out.println("¡Eso no está en el menu! Intentalo otra vez...");  
+			}
+			}while (n<=0);
+			
+		}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+
