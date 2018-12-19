1b6de7687b0d1a6af0e2da7a327fc9db377dedf1
alumne-DAM <alumne-DAM@172.16.26.91>
2018-12-19 16:53:48 +0100

    Jje

--- David/src/pru03/E02/PRU03E02Cotxe_David_Sanchez.java
+++ David/src/pru03/E02/PRU03E02Cotxe_David_Sanchez.java
@@ -1,8 +1,13 @@
 package pru03.E02;
 
+import java.util.Scanner;
+
 public class PRU03E02Cotxe_David_Sanchez extends CotxeAbstracte implements InterfaceCotxe{
 
 	protected EstatsMotorCotxe estatsmotor;
+
+	protected String auto[] = {"F","N","R"};
+
 
 	public PRU03E02Cotxe_David_Sanchez(String marca, String model, TipusCanvi tipuscanvi) {
 		super(marca, model, tipuscanvi);
@@ -62,4 +67,86 @@ public class PRU03E02Cotxe_David_Sanchez extends CotxeAbstracte implements Inter
 			throw new IllegalArgumentException ("El coche ya esta apagado.");		
 		}		
 	}
+
+	public void CanviarMarxaAutomatic() throws Exception {
+
+		int menuu;
+		int nn= 0;
+		String valor = "N";
+		Scanner teclado = new Scanner(System.in);	
+
+
+		if (this.tipuscanvi.equals(TipusCanvi.CanviAutomatic)) {
+
+			do {
+
+				System.out.println ("1- Cambiar a neutra");
+				System.out.println ("2- Cambiar a reverse");
+				System.out.println ("3- Cambiar forward");
+				System.out.println ("4- Marchas cambiadas.");
+
+				menuu = teclado.nextInt();
+
+				if (menuu==1) {
+
+					valor = auto[1];
+					System.out.println ("Has cambiado a la marcha " + valor);
+
+				}
+				else if (menuu==2) {
+
+					valor = auto[2];
+					System.out.println ("Has cambiado a la marcha " + valor);
+
+				}
+				else if (menuu==3) {
+
+					valor = auto[0];
+					System.out.println ("Has cambiado a la marcha " + valor);
+
+				}
+				else if (menuu==4) {
+
+
+					System.out.println("Te habras quedado en modo" + valor); 
+					nn++;
+				}
+
+				else{
+
+					System.out.println("¡Eso no está en el menu! Intentalo otra vez...");  
+				}
+			}while (nn<=0);
+
+
+		} else { 
+
+			throw new IllegalArgumentException ("El coche es manual, no puedes cambiar marchas automaticas.");
+
+		}	
+	}
+
+
+
+	public void CanviarMarxaManual() throws Exception {
+
+		if (estatsmotor.equals(EstatsMotorCotxe.EnMarxa)) {
+
+			estatsmotor = EstatsMotorCotxe.Aturat;
+
+		} else {
+
+			throw new IllegalArgumentException ("El coche ya esta apagado.");		
+		}		
+	}
+
+
+
+
+
+
+
+
+
+
 }
