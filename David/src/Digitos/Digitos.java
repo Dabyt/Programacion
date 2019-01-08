8f6e48d5f083008dc602247ff25711c91a69ec2f
alumne-DAM <alumne-DAM@172.16.26.142>
2019-01-08 16:58:26 +0100

    digitos

---
+++ David/src/Digitos/Digitos.java
@@ -0,0 +1,18 @@
+package Digitos;
+
+import java.util.Scanner;
+
+public class Digitos {
+
+	public static void main(String[] args) {
+		
+	int valor;
+	Scanner teclado = new Scanner(System.in);		
+		
+	valor = teclado.nextInt();	
+		
+
+
+	}
+
+}
