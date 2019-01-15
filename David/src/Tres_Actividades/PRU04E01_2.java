72b4ea07ca05150d1dbe2f9cf15ae5ad6c9081ec
alumne-DAM <alumne-DAM@172.16.26.142>
2019-01-15 15:55:56 +0100

    Ejercicios tope guapos

--- David/src/Tres_Actividades/PRU04E01_2.java
+++ David/src/Tres_Actividades/PRU04E01_2.java
@@ -4,14 +4,14 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Collections;
 
-public class PRU04E01_1 {
+public class PRU04E01_2 {
 
 	public static void main(String[] args) {
-
+	
 		try{
 
 			BufferedReader bf = new	BufferedReader (new FileReader(args[0]));
@@ -51,4 +51,28 @@ public class PRU04E01_1 {
 			// https://jarroba.com/arraylist-en-java-ejemplos/
 		}catch(IOException e) {
 
-		}}}
+		}}}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
