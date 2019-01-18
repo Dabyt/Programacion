925bfeaf2b6c02dd03bca5ffcf85bc25edba63d1
alumne-DAM <alumne-DAM@172.16.26.142>
2019-01-18 16:55:30 +0100

    Casi acabado

--- David/src/PRU04/PRU04_David_Sanchez.java
+++ David/src/PRU04/PRU04_David_Sanchez.java
@@ -1,31 +1,26 @@
-package PRU04;
+
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class PRU04_David_Sanchez {
 
 	
-	public String pasarParaula (String linea, int pos_inicial, int pos_final) {
+	public static String pasarParaula (String linea, int pos_inicial, int pos_final) {
 		
 		String lineaEditada = "";
 		
-		for (int i=0;i<linealeida.length();i++) {
+		for (int i=pos_inicial;i<pos_final;i++) {
 			
-			if (linealeida.charAt(i) == 32) {
-				
-				
-				
-			}
-				
-				
-			}
+			if (linea.charAt(i) != 32) {
+				lineaEditada += linea.charAt(i);	
+			}}
 		
-		
+		lineaEditada += ";";
 		
 		return lineaEditada;
 	}
@@ -37,29 +32,27 @@ public class PRU04_David_Sanchez {
 			BufferedReader br = new	BufferedReader (new FileReader(args[0]));
 			BufferedWriter bw = new	BufferedWriter (new FileWriter(args[1]));
 
-			ArrayList<String> lineacompleta = new ArrayList<String>();
-			String[] linea;
+			//ArrayList<String> lineacompleta = new ArrayList<String>();
+			//String[] linea;
+			String variable = "";
 			String linealeida = "";
-			int posicion_a = 0;
-			int posicion_b = 0;
-			int posicion_c = 0;
-			int posicion_d = 0;
-			int n=0;
-			
-			
+		
 			do {
 				
 				linealeida = br.readLine();
 				
 				if (linealeida !=null) {
 					
+				variable += pasarParaula(linealeida,0,5);	
+				variable += pasarParaula(linealeida,6,15);
+				variable += pasarParaula(linealeida,16,26);
+				variable += pasarParaula(linealeida,27,40);
+				variable += pasarParaula(linealeida,41,47);
+				variable += pasarParaula(linealeida,48,57);
 				
-					
-					
+				bw.write(variable + "\n");
 				}
-				
-				
-				
+					
 			}while(linealeida !=null);
 					
 			br.close();
