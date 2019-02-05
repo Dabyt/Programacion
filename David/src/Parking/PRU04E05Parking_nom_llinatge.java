2be63115ce77cf0ef4077f93bead80e759d76955
alumne-DAM <alumne-DAM@172.16.26.142>
2019-02-05 17:49:09 +0100

    metodos

---
+++ David/src/Parking/PRU04E05Parking_nom_llinatge.java
@@ -0,0 +1,68 @@
+package Parking;
+
+public class PRU04E05Parking_nom_llinatge {
+
+	public PRU04E05Parking_nom_llinatge (int places_no_discapacitats, int places_discapacitats) {
+		
+	
+		
+		
+		
+	}
+	
+	
+	
+	public void llegirMatricules(String path) throws Exception{	
+		
+	}
+	
+	
+	
+	public int entrarCotxe(String maricula)throws Exception{
+			
+	}
+	
+	public int entrarCotxeDiscapacitat(String maricula)throws Exception{
+			
+	}
+	
+	
+	
+	
+	public void surtCotxe(String maricula)throws Exception{
+		
+	}
+
+	public void surtrCotxeDiscapacitat(String maricula)throws Exception{
+	
+	}
+	
+	
+	
+	
+	public int getPlacesOcupades(TipusPlacesParking tipus)throws Exception{
+		
+	}
+	public int getPlacesLliures(TipusPlacesParking tipus)throws Exception{	
+		
+	}
+	
+	
+	
+	
+	public void guardarMatricules(String path)throws Exception{
+		
+	}
+	
+	
+	
+   }
+	
+	
+	
+	
+		
+
+	
+
+
